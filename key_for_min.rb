72b0b25b9da40a8d9c64adf634bed089c22e08b1
Kyle Andruczk <kyle.andruczk@gmail.com>
2021-05-26 23:28:52 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,7 +8,7 @@ def key_for_min_value(name_hash)
   min_val = 10000000000
   min_key = ""
   name_hash.each do |key, val|
-    if val < min_val
+    if name_hash[:key] < min_val
       min_key = key
     end
   end
