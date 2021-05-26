539fa79ea774527a98d81639f620399bf831ee5b
Kyle Andruczk <kyle.andruczk@gmail.com>
2021-05-26 23:30:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   if name_hash == {}
     return nil
   end
-  min_val = 10000000000
+  min_val = 100
   min_key = ""
   name_hash.each do |key, val|
     if val < min_val
